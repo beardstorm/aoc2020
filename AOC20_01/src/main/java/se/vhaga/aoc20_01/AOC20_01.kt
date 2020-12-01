aae00a915dc5ef08fbbcab6f6dacff41cd3fa76c
Viktor Haga <viktor.haga@appshack.se>
2020-12-01 18:34:07 +0100

    Add solution for Day 1

---
+++ AOC20_01/src/main/java/se/vhaga/aoc20_01/AOC20_01.kt
@@ -0,0 +1,49 @@
+package se.vhaga.aoc20_01
+
+import java.io.File
+import java.nio.file.Paths
+
+fun main(args: Array<String>) {
+    AOC20_01.run()
+}
+
+object AOC20_01 : Runnable {
+
+    private val input by lazy {
+        val assetsDir = Paths.get("").toAbsolutePath().toString() + "/AOC20_01/src/main/assets"
+        val inputFile = File(assetsDir, "input.txt")
+
+        inputFile.readLines().mapNotNull { it.toIntOrNull() }
+    }
+
+    override fun run() {
+        println("The answer to part one is: ${partOne(input)}")
+        println("The answer to part two is: ${partTwo(input)}")
+    }
+
+    private fun partOne(input: List<Int>): Int {
+        input.forEach { outer ->
+            input.filter { it != outer }.forEach { inner ->
+                if (outer + inner == 2020) {
+                    return outer * inner
+                }
+            }
+        }
+
+        return 0
+    }
+
+    private fun partTwo(input: List<Int>): Int {
+        input.forEach { outer ->
+            input.filter { it != outer }.forEach { inner ->
+                input.filter { it != outer || it != inner }.forEach { innermost ->
+                    if (outer + inner + innermost == 2020) {
+                        return outer * inner * innermost
+                    }
+                }
+            }
+        }
+
+        return 0
+    }
+}
